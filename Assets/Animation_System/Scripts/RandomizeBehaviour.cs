e36c874ec006ac363e7d54a5ea27da11c7d7cecf
dkITMax <91422826+MaddestMax98@users.noreply.github.com>
2023-01-20 19:45:20 +0000

    Removed broken animations, fixed Serialization bug

--- Assets/Animation_System/Scripts/RandomizeBehaviour.cs
+++ Assets/Animation_System/Scripts/RandomizeBehaviour.cs
@@ -1,24 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class BoredBehaviour : StateMachineBehaviour
+public class RandomizeBehaviour : StateMachineBehaviour
 {
     [SerializeField]
-    [Tooltip("Time it takes for the character to play a bored animation")]
-    private float _patience;
+    [Tooltip("Time it takes for the character to play an alternative animation")]
+    private float _timeTillSwitch;
 
     [SerializeField]
     [Tooltip("Total animations added to the blend tree")]
     private int _totalAnimations;
+
+    [SerializeField]
+    [Tooltip("Name of the variable that we trying to change the value of during runtime")]
+    private string triggerVariable;
 
     private bool _isBored;
     private float _idleTime;
     private float _animationToPlay;
 
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
+    { 
         ResetAnimation();
     }
 
@@ -28,13 +29,13 @@ public class BoredBehaviour : StateMachineBehaviour
         {
             _idleTime += Time.deltaTime;
 
-            if (_patience < _idleTime && stateInfo.normalizedTime % 1 < 0.02f)
+            if (_timeTillSwitch < _idleTime && stateInfo.normalizedTime % 1 < 0.02f)
             {
                 _animationToPlay = Random.Range(1, _totalAnimations + 1);
                 _animationToPlay = _animationToPlay * 2 - 1;
                 _isBored = true;
 
-                animator.SetFloat("CurrentIdleAnimation", _animationToPlay - 1);
+                animator.SetFloat(triggerVariable, _animationToPlay - 1);
             }
         }
         else if (stateInfo.normalizedTime % 1 > 0.98)
@@ -42,7 +43,7 @@ public class BoredBehaviour : StateMachineBehaviour
             ResetAnimation();
         }
 
-        animator.SetFloat("CurrentIdleAnimation", _animationToPlay, 0.4f, Time.deltaTime);
+        animator.SetFloat(triggerVariable, _animationToPlay, 0.4f, Time.deltaTime);
     }
 
     private void ResetAnimation()
